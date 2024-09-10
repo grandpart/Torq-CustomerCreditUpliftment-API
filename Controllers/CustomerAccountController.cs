fdb206885f453741cfc4869af71486f3fd7f57c6
Nkosinathi <nkosinathin@grandmark.co.za>
2024-09-10 08:27:49 +0200

    Fixed the error on for serializing a Json object to a model on the customer details controller

--- Controllers/CustomerAccountController.cs
+++ Controllers/CustomerAccountController.cs
@@ -1,22 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using gmTemporaryCustomerCreditLimit.Data;
+﻿using gmTemporaryCustomerCreditLimit.Data;
 using gmTemporaryCustomerCreditLimit.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 
 namespace gmTemporaryCustomerCreditLimit.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CustomerController : Controller
+    public class CustomerAccountController : Controller
     {
 
         [HttpGet]
         [Route("GetCustomerDetails/{customerAccount}")]
-        public Task GetCustomerDetailsByAccountNo( string customerAccount)
+        public Task GetCustomerDetailsByAccountNo(string customerAccount)
         {
             var myConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            
+
             string connSyspro = myConfig.GetValue<string>("ConnectionStrings:Syspro") ?? string.Empty;
 
             var customer = CustomerDetailsData.GetCustomerDetailsByAccountNo(connSyspro, customerAccount);
@@ -24,18 +24,19 @@ namespace gmTemporaryCustomerCreditLimit.Controllers
             if (customer != null)
             {
                 Response.StatusCode = StatusCodes.Status200OK;
-                return Response.WriteAsync(JsonConvert.SerializeObject(customer));
+
             }
             else
             {
                 Response.StatusCode = StatusCodes.Status404NotFound;
-                return Response.WriteAsync(JsonConvert.SerializeObject(customer));
-            }
 
+            }
+            return Response.WriteAsJsonAsync<CustomerDetails>(customer);
+            //return Response.WriteAsync(JsonConvert.SerializeObject(customer));
         }
         [HttpGet]
         [Route("GetCustomerDetailsPerUserBranch/{username}/{branch}")]
-        public Task GetCustomerDetailsPerBranch(int username, string branch )
+        public Task GetCustomerDetailsPerBranch(int username, string branch)
         {
             var myConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
