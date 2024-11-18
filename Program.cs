195384b98e7f16d783960c18e7a2135168ca4b8a
Nkosinathi Nkosi <NkosinathiN@grandmark.co.za>
2024-11-18 15:11:59 +0200

    Publish Configs

--- Program.cs
+++ Program.cs
@@ -12,12 +12,26 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger/json ", "Torq - Credit Upliftment");
+});
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger/index.html");
+        return;
+    }
+    await next();
+});
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
